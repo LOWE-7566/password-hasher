96caded77ddc4c8987b5f19c545fc0294af687ee
lowecainandaya <loweandaya7566@gmail.com>
2023-03-02 16:31:50 +0800

    bug fixed

--- password.test.ts
+++ password.test.ts
@@ -2,10 +2,10 @@ import Hasher from "./password.js";
 const password = new Hasher(15);
 
 // hash 
-password.hash("takataka").then((__hash:any) => {
+password.hash("sample password").then((__hash:any) => {
   console.log(__hash)})
 
-password.compare("takataka","2b77e37044d70a18be0236d5f57036899447bc5efec1f02409ea5a31d9068342").then((v:any) => {
-  console.log(v);
+password.compare("sample password","2b77e37044d70a18be0236d5f57036899447bc5efec1f02409ea5a31d9068342").then((result:any) => {
+  console.log(result);
 } )
 
